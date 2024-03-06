71cbf34eb6e33e0bfcff22e89e8c850e72d87e2d
Jianxing Weijx <weijx.cpp@gmail.com>
2024-03-06 09:24:42 +0800

    add pm logger fix logger bugs

--- pm/views.py
+++ pm/views.py
@@ -1,7 +1,8 @@
 import logging
 
 from django.core.serializers import json
-from django.shortcuts import render
+from django.db.models import F
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 from django.utils import timezone
@@ -11,8 +12,17 @@ from pm.models import ProjectInfo
 from pmmonkey.entities.models import *
 from django.http.response import HttpResponse, JsonResponse
 from django.conf import  settings
-logger = logging.getLogger(__name__)
+#取 来自setting中配置
+
+logger = logging.getLogger('django')
+# logger.setLevel(logging.INFO)
 def add(request):
+    # logger.setLevel(logging.INFO)
+    # print('===== test add ')
+    # print(request.body)
+    logger.info("**************************-----add a new project.")
+
+    logger.info(request.body)
     resp = {"status":000,"message":'项目创建成功'}
     if request.method == 'POST':
         try:
@@ -21,13 +31,18 @@ def add(request):
             logger.debug(request_data)
             pm = ProjectInfo()
 
+
             pmdict = json.loads(request_data)
             pm.addPM(pmdict)
         except json.JSONDecodeError:
             return JsonResponse({'error': 'Invalid JSON data'}, status=400)
     logger.info('添加项目信息')
-    pmInfo = request.body
-    pm = ProjectInfo()
-    pm.addPM(pmInfo)
 
     return  JsonResponse(resp, status=200)
+
+def update_satus(request):
+    ProjectInfo.objects.filter(projectId=request.projectId).update(status=F(request.status))
+
+    # q = get_object_or_404(ProjectInfo, pk=request.id)
+    # q.status = request.status
+    # q.save()
